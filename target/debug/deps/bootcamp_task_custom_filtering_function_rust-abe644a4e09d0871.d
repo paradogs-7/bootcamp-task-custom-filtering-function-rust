a11be80cfbb13f36ee7fb6d6f397e43601e08792
paradogs-7 <vnyz2002@gmail.com>
2025-04-28 11:23:39 +0300

    Task done

---
+++ target/debug/deps/bootcamp_task_custom_filtering_function_rust-abe644a4e09d0871.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-custom-filtering-function-rust/target/debug/deps/libbootcamp_task_custom_filtering_function_rust-abe644a4e09d0871.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-custom-filtering-function-rust/target/debug/deps/bootcamp_task_custom_filtering_function_rust-abe644a4e09d0871.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bootcamp_task_custom_filtering_function_rust-e440a16414e87976.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-custom-filtering-function-rust/target/debug/deps/libbootcamp_task_custom_filtering_function_rust-e440a16414e87976.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-custom-filtering-function-rust/target/debug/deps/bootcamp_task_custom_filtering_function_rust-e440a16414e87976.d: src/main.rs
+
+src/main.rs:
